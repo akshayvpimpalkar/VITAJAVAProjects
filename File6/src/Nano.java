818f39960e78ddad2c7e254e907326540f43e611
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-06-09 23:52:00 +0530

    initial commit

---
+++ File6/src/Nano.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+class Car{
+	
+}
+public class Nano extends Car implements Serializable{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
