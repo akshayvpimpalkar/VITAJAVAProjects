a398a9ce26af431e49c78922417ac57c7e1f259b
Akshay Pimpalkar <akshay.pimpalkar@gmail.com>
2017-06-10 00:19:46 +0530

    initial commit

---
+++ Th5/src/Main.java
@@ -0,0 +1,51 @@
+class Th1 implements Runnable{
+
+	@Override
+	 public void run() {
+		// TODO Auto-generated method stub
+		for(int i = 0; i <= 50; i++){
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+class Th2 implements Runnable{
+
+	@Override
+	 public void run() {
+		// TODO Auto-generated method stub
+		for(int i = 50; i >= 0; i--){
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+}
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Th1 th1 = new Th1();
+		Th2 th2 = new Th2();
+		
+		Thread t1 = new Thread(th1);
+		Thread t2 = new Thread(th2);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
